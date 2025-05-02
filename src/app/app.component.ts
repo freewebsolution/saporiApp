c1caf2274ff8aef9624eca4523419c74098c7468
lucio ticali <luciano.ticali@technocenter.it>
2025-05-02 17:54:05 +0200

    Aggiunto navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone :true,
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'SaporiApp';
+
 }
