cd5e225b843cfc55470f5d113753604c4eeb9b67
lucio ticali <luciano.ticali@technocenter.it>
2025-05-05 07:46:15 +0200

    Metodo Getall

--- src/app/categories/category.service.ts
+++ src/app/categories/category.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class CategoryService {
   constructor(private http: HttpClient) { }
 
   public getAll() {
-    return this.http.get("http://localhost:3700/categories")
+    return this.http.get(`${environment.api}categories`)
   }
 }
