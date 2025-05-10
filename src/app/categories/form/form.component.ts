c72d4f9a3da37fae3f560f4e3137ceb2087e8e44
lucio ticali <luciano.ticali@technocenter.it>
2025-05-10 10:04:09 +0200

    Realizza il form Reattivo

--- src/app/categories/form/form.component.ts
+++ src/app/categories/form/form.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject } from "@angular/core";
+import { FormBuilder, ReactiveFormsModule, Validators } from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatInputModule } from "@angular/material/input";
+import { MatCardModule } from "@angular/material/card";
 
 @Component({
-  selector: 'category-form',
-  imports: [],
-  templateUrl: './form.component.html',
-  styles: ``
+  selector: "category-form",
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatCardModule,
+  ],
+  templateUrl: "./form.component.html",
+  styles: ``,
 })
 export class CategoryFormComponent {
-
+  private fb = inject(FormBuilder);
+  categoryForm = this.fb.group({
+    id: [null],
+    name: ["", Validators.required],
+    description: ["", Validators.required],
+  })
 }
